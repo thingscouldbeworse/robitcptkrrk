5066037a008f20294544e61abf496d900400bbac
thingscouldbeworse <megal33tdude@gmail.com>
2016-12-22 21:42:10 -0500

    got a big text chunk now

--- generate.py
+++ generate.py
@@ -1,3 +1,19 @@
 import markovify
+import configparser
+import twitter
+import json
+import re
 
+with open( 'tweetDB.json' ) as json_data:
+	all_tweets = json.load(json_data)
+	json_data.close()
 
+text_chunk = ""
+for tweet in all_tweets:
+#	print( tweet['text'][:3] + "|" )
+	if( tweet['text'][:3] != 'RT ' and tweet['text'][:3] != ' RT' ):
+		text = re.sub( '\@.*?[ ]', '', tweet['text'] )
+		if( text[-1] != '.' and text[-1] != '!' and text[-1] != '?' ):
+			text_chunk = text_chunk + " " + text + '.'
+
+print( text_chunk )
