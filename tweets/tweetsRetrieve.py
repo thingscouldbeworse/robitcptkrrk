af975cf2f46f446797c1f687e7dbad1b2aa908a8
thingscouldbeworse <megal33tdude@gmail.com>
2017-03-01 19:42:43 -0500

    changed heirarchy of tweets directory, restructured tweet retrieval

---
+++ tweets/tweetsRetrieve.py
@@ -0,0 +1,73 @@
+import markovify
+import configparser
+import twitter
+import os
+
+def connection_init():
+
+	cfg = configparser.ConfigParser()
+	cfg.read('config.ini')
+
+	ACCESS_TOKEN = cfg.get('twitter', 'ACCESS_TOKEN') 
+	ACCESS_SECRET = cfg.get('twitter', 'ACCESS_SECRET')
+	CONSUMER_KEY = cfg.get('twitter', 'CONSUMER_KEY') 
+	CONSUMER_SECRET = cfg.get('twitter', 'CONSUMER_SECRET')
+
+	oauth = twitter.oauth.OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+	# Initiate the connection to Twitter REST API
+	twitter_api = twitter.Twitter(auth=oauth)
+
+	return twitter_api
+
+def getAllTweets():
+
+	twitter_api = connection_init()
+
+	# get initial 50
+	all_tweets = []
+	statuses = twitter_api.statuses.user_timeline(screen_name='CptKrrk', count=50, include_retweets=True )
+	all_tweets = all_tweets + statuses
+	results = len(statuses)
+	index = 0
+	while results >= 50:
+		statuses = twitter_api.statuses.user_timeline(screen_name='CptKrrk', count=50, include_retweets=True, max_id=statuses[49]['id'])
+		all_tweets = all_tweets + statuses
+		results = len(statuses)
+		print( index )
+		index = index + 1
+
+	all_tweets = sorted(all_tweets,key=lambda l:l['id'], reverse=False)
+
+	return all_tweets
+
+def storeTweets( json_content, itemType ):
+
+	with open( os.getcwd() + '/tweets/' + itemType + 'DB.json', 'w' ) as DB_file:
+		DB_file.write( json.dumps(json_content, indent=2) )
+
+def consolidate( content_list, itemType ):
+
+	with open( os.getcwd() + '/tweets/' + itemType + 'DB.json' ) as json_data:
+		all_content = json.load(json_data)
+		json_data.close()
+
+	print( str(len( all_content )) + " " + itemType + " in DB to start" )
+
+	for content_json in content_list:
+		for item in content_json:
+			if item not in all_content:
+			
+				all_content.append( item )
+			
+
+	print( str(len( all_content )) + " " + itemType + " now in DB" )
+
+	return all_content
+
+def getMentions():
+
+	twitter_api = connection_init()
+
+	mentions = twitter_api.statuses.mentions_timeline()
+
+
